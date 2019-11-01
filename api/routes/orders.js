9384cdc2f0b5afd9bfb029d3a8c23e5938d011ba
trips2503 <tprasad2@horizom.csueastbay.edu>
2019-10-31 19:17:35 -0700

    second commit

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,15 +1,73 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 router.get('/', (req,res,next) => {
-    res.status(200).json({
-        message : 'Orders fetched'
+    Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then(docs=>{
+        res.status(200).json({
+            count: docs.length,
+            order: docs.map(doc=>{
+                return{
+                    _id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders'+doc._id
+                    }
+                }
+            }),
+        });
+    })
+    .catch(err=>{
+        res.status(500).json({
+            error:err
+        });
     });
 });
 
 router.post('/', (req,res,next) => {
-    res.status(201).json({
-        message : 'Order was created'
+    Product.findById(req.body.productId)
+    .then()
+    .catch(err=>{
+        res.status(500).json({
+            message:'Product not found',
+            error: err
+        });
+    });
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order
+    .save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Order Stored',
+            createdOrder: {
+                _id: result._id,
+                product: result.product,
+                quantity: result.quantity
+            },
+            request: {
+                type:'GET',
+                url:  'http://localhost:3000/orders'+result._id
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect('mongodb+srv://mdbuser:mongodb@cluster0-4uha3.mongodb.net/test?retryWrites=true&w=majority');
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//adding headers to the responses
+app.use((req,res,next)=>{
+    res.header('Access-Control-Aloow-Origin','*');
+    res.header('Access-Control-Allow-Headers',"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method ==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH ,DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //Routes handling products and orders
 app.use('/products', productRoutes);
